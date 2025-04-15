a9e94166a551342f7266e7b8a827f30c6e002272
Eman <emanabukhater2001@gmail.com>
2025-04-15 19:14:58 +0300

    create home page

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </Router>
   );
