243547bf25c463ea4ce31837ba39720ddb869a4c
Eman <emanabukhater2001@gmail.com>
2025-04-16 19:47:55 +0300

    Update ProductCard.jsx to Add Items to Cart

--- ecommerce-app/src/components/ProductCard.jsx
+++ ecommerce-app/src/components/ProductCard.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext"; // import context
 
 export default function ProductCard({ product }) {
+  const { addToCart } = useContext(CartContext); // use context
   return (
     <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition duration-400">
       <img
@@ -8,7 +10,7 @@ export default function ProductCard({ product }) {
         alt={product.title}
         className="h-48 object-contain w-full mb-4"
       />
-      <h2 className="text-md font-semibold line-clamp-2 mb-2">
+      <h2 className="text-md w-1/2 font-semibold line-clamp-2 mb-2">
         {product.title}
       </h2>
       <p className="text-indigo-500 font-bold text-lg">${product.price}</p>
@@ -18,7 +20,10 @@ export default function ProductCard({ product }) {
       <div className="flex items-center gap-1 text-yellow-500 text-sm">
         ⭐ {product.rating.rate} ({product.rating.count})
       </div>
-      <button className="mt-4 w-full bg-indigo-500 text-white py-2 rounded hover:bg-indigo-600 transition">
+      <button
+        className="mt-4 w-full bg-indigo-500 text-white py-2 rounded hover:bg-indigo-600 transition"
+        onClick={() => addToCart(product)} // call addToCart
+      >
         Add to Cart
       </button>
     </div>
