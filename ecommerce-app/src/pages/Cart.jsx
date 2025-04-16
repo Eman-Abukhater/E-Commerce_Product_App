9bea8cc4634062911a1329b7a60b072d66512f76
Eman <emanabukhater2001@gmail.com>
2025-04-16 23:18:22 +0300

    start create cart page

--- ecommerce-app/src/pages/Cart.jsx
+++ ecommerce-app/src/pages/Cart.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
-function Cart() {
+export default function Cart() {
+  const {
+    cartItems,
+    removeFromCart,
+    cartTotal,
+    cartCount,
+    updateQuantity,
+  } = useContext(CartContext);
+
   return (
-    <div>Cart</div>
-  )
+   <div></div>
+  );
 }
-
-export default Cart
