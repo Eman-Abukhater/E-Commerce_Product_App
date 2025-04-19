d87a5124f21fc5dd373bd4a71e790a639d0b1b12
Eman <emanabukhater2001@gmail.com>
2025-04-19 19:24:06 +0300

    Fetch product info by ID inside Product Details page

--- ecommerce-app/src/pages/ProductDetails.jsx
+++ ecommerce-app/src/pages/ProductDetails.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
-  return (
-    <div>ProductDetails</div>
-  )
+  // Fetch product details using the id
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // fetching product details
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data = await res.json();
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+        setLoading(false);
+      }
+    };
+    fetchProduct();
+  }, [id]);
+
+  return <div>ProductDetails</div>;
 }
 
-export default ProductDetails
+export default ProductDetails;
