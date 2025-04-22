b45dfb4466006ebb401bfb9d4ffbd1560a8080af
Eman <emanabukhater2001@gmail.com>
2025-04-22 20:43:16 +0300

    add Cancel page

---
+++ ecommerce-app/src/pages/Cancel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cancel = () => {
+  return (
+    <div className="text-center mt-20">
+      <h2 className="text-3xl text-red-500 font-bold mb-4">❌ Payment Cancelled</h2>
+      <p className="text-lg">No worries. You can try again anytime.</p>
+    </div>
+  );
+};
+
+export default Cancel;
