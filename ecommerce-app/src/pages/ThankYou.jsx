408d974d0d54e58604550cbbe0e8a4c15f2b032f
Eman <emanabukhater2001@gmail.com>
2025-04-22 20:39:56 +0300

    add Thank You page

---
+++ ecommerce-app/src/pages/ThankYou.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <div className="text-center mt-20">
+      <h2 className="text-3xl text-green-600 font-bold mb-4">🎉 Payment Successful!</h2>
+      <p className="text-lg">Thank you for your purchase.</p>
+    </div>
+  );
+};
+
+export default ThankYou;
